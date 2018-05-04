3e53e5492d78e5bd80bef9c6930ca2779d33f556
princessguerrero <princessguerrero@ac.c4q.nyc>
2018-05-04 12:41:34 -0400

    edit frontend to front-end

--- backend/app.js
+++ backend/app.js
@@ -25,12 +25,12 @@ app.use(session({
 })); 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../front-end/build')));
 
 // app.use('/', index);
 app.use('/users', users);
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname + '../front-end/build/index.html'));
 });
 
 // catch 404 and forward to error handler
